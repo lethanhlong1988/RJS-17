97a1288870f1b66983bb27cfcdcc8b58f59e6afa
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-03-26 08:39:32 +0000

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,18 @@
 export default function Login() {
   return (
-    <form>
+    <form action="">
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input type="email" id="email" name="email" />
         </div>
-
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input type="password" id="password" name="password" />
         </div>
       </div>
-
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
         <button className="button">Login</button>
